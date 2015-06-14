822f0ae2478528a2a3868ae0b0572f3acecf305a
Geoffrey Frogeye <geoffrey@frogeye.fr>
2015-06-14 23:50:26 +0200

    Dynamic commands

--- public/commands.js
+++ public/commands.js
@@ -1,58 +1,188 @@
 var globalCommands = [{
-    name: "Forbid movement",
-    cmd: 'c_speedmult(0)',
+    name: "List all players",
+    cmd: 'c_listallplayers()',
 }, {
-    name: "Allow movement",
-    cmd: 'c_speedmult(1)',
+    name: "Kick %player%",
+    cmd: 'TheNet:Kick("%player%")',
+    args: [{
+        'name': 'player',
+        'type': 'text',
+        'placeholder': 'player',
+    }],
+}, {
+    name: "Ban %player%",
+    cmd: 'TheNet:Ban("%player%")',
+    args: [{
+        'name': 'player',
+        'type': 'text',
+        'placeholder': 'player',
+    }],
+}, {
+    name: "Spanw %amount% %prefab%",
+    cmd: 'c_spawn("%prefab%", %amount%)',
+    args: [{
+        'name': 'prefab',
+        'type': 'text',
+        'placeholder': 'prefab',
+    }, {
+        'name': 'amount',
+        'type': 'number',
+        'value': 1,
+        'min': 1,
+        'step': 1,
+    }],
+}, {
+    name: "Give %amount% %prefab%",
+    cmd: 'c_give("%prefab%", %amount%)',
+    args: [{
+        'name': 'prefab',
+        'type': 'text',
+        'placeholder': 'prefab',
+    }, {
+        'name': 'amount',
+        'type': 'number',
+        'value': 1,
+        'min': 1,
+        'step': 1,
+    }],
+}, {
+    name: "Set health to %percent%",
+    cmd: 'c_sethea​lth(%percent%)',
+    args: [{
+        'name': 'percent',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 1,
+        'step': 0.01,
+    }],
+}, {
+    name: "Set sanity to %percent%",
+    cmd: 'c_setsanity(%percent%)',
+    args: [{
+        'name': 'percent',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 1,
+        'step': 0.01,
+    }],
+}, {
+    name: "Set hunger to %percent%",
+    cmd: 'c_sethunger(%percent%)',
+    args: [{
+        'name': 'percent',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 1,
+        'step': 0.01,
+    }],
 }, {
     name: "Toggle god mode",
     cmd: 'c_godmode()',
 }, {
-    name: "List all players",
-    cmd: 'c_listallplayers()',
+    name: "Multiply speed by %speed%",
+    cmd: 'c_speedmult(%speed%)',
+    args: [{
+        'name': 'speed',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'step': 1,
+    }],
+    autoEx: true,
 }, {
-    name: "Skip day",
-    cmd: '​TheWorld:PushEvent("ms_nextcycle")',
+    name: "Skip %days% day",
+    cmd: '​LongUpdate(%days%*TUNING.TOTAL_DAY_TIME)',
+    args: [{
+        'name': 'days',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'step': 0.01,
+    }]
 }, {
     name: "Skip phase",
     cmd: 'TheWorld:PushEvent("ms_nextphase")',
 }, {
-    name: "Start summer",
-    cmd: 'TheWorld:PushEvent("ms_setseason", "summer")',
+    name: "Set segments %day% + %dusk% + %night% = 16",
+    cmd: '​TheWorld:PushEvent("ms_setclocksegs", {day=%day%,dusk=%dusk%,night=%night%})',
+    args: [{
+        'name': 'day',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 16,
+        'step': 1,
+    }, {
+        'name': 'dusk',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 16,
+        'step': 1,
+    }, {
+        'name': 'night',
+        'type': 'number',
+        'value': 1,
+        'min': 0,
+        'max': 16,
+        'step': 1,
+    }]
 }, {
-    name: "Start winter",
-    cmd: 'TheWorld:PushEvent("ms_setseason", "winter")',
+    name: "Start %season%",
+    cmd: 'TheWorld:PushEvent("ms_setseason", "%season%")',
+    args: [{
+        'type': 'select',
+        'name': 'season',
+        'options': ['autumn', 'winter', 'spring', 'summer']
+    }],
+    autoEx: true,
 }, {
-    name: "Start rain",
-    cmd: 'TheWorld:PushEvent("ms_forceprecipitation")',
+    name: "%activated% Rain",
+    cmd: 'TheWorld:PushEvent("ms_forceprecipitation", %activated%)',
+    args: [{
+        'name': 'activated',
+        'type': 'checkbox',
+        'value': 1
+    }],
+    autoEx: true,
 }, {
-    name: "Stop rain",
-    cmd: 'TheWorld:PushEvent("ms_forceprecipitation", false)',
+    name: "%activated% Allow incoming connections",
+    cmd: 'TheNet:SetAllowIncomingConnections(%activated%)',
+    args: [{
+        'name': 'activated',
+        'type': 'checkbox',
+        'value': 1,
+        'checked': true
+    }],
+    autoEx: true,
 }, {
-    name: "Reset day",
-    cmd: 'c_reset(false)',
-}, {
-    name: "Reload world",
-    cmd: 'c_reset(true)',
-}, {
-    name: "Regenerate world",
-    cmd: 'c_regenerateworld()',
+    name: "Clear morgue",
+    cmd: 'ErasePersistentString("morgue")',
 }, {
     name: "Save the server",
     cmd: 'c_save()',
 }, {
-    name: "Stop server without saving",
-    cmd: 'c_shutdown(false)',
+    name: "Reset server (%save%save)",
+    cmd: 'c_reset(%save%)',
+    args: [{
+        'name': 'save',
+        'type': 'checkbox',
+        'value': 1,
+        'checked': true
+    }],
 }, {
-    name: "Save and stop the server",
-    cmd: 'c_shutdown(true)',
+    name: "Regenerate world",
+    cmd: 'c_regenerateworld()',
 }, {
-    name: "Allow incoming connections",
-    cmd: 'TheNet:SetAllowIncomingConnections(true)',
-}, {
-    name: "Forbid incoming connections",
-    cmd: 'TheNet:SetAllowIncomingConnections(false)',
-}, {
-    name: "Clear morgue",
-    cmd: 'ErasePersistentString("morgue")',
+    name: "Stop server (%save%save)",
+    cmd: 'c_shutdown(%save%)',
+    args: [{
+        'name': 'save',
+        'type': 'checkbox',
+        'value': 1,
+        'checked': true
+    }],
 }];
--- public/main.js
+++ public/main.js
@@ -55,20 +55,71 @@ $(function () {
         return false;
     });
 
-    function exec(e) {
-        input($(e.target).data('cmd').cmd);
+    function execCmd(e) {
+        var form = $(e.target).parent();
+        var cmd = '';
+        var isText = true;
+        var cmdSplit = form.data('cmd').cmd.split('%');
+        var valuesAr = form.serializeArray();
+        var values = {};
+        Object.keys(valuesAr).map(function (value, index) {
+            value = valuesAr[index];
+            values[value.name] = value.value;
+        });
+        for (var cdI in cmdSplit) {
+            var cd = cmdSplit[cdI];
+            if (isText) {
+                cmd += cd;
+            } else {
+                cmd += values[cd] ? values[cd] : 'false';
+            }
+            isText = !isText;
+        }
+        input(cmd);
     }
+
     for (var cmdI in globalCommands) {
         var cmd = globalCommands[cmdI];
-        var button = $('<input>')
-            .attr('type', 'button')
-            .val('Execute')
-            .data('cmd', cmd)
-            .click(exec);
-        $('#globalCommands').append(
-            $('<div>')
-            .append(cmd.name + ' ')
-            .append(button)
-        );
+        var form = $('<form>').data('cmd', cmd).submit(function() { return false });
+        if (cmd.args) {
+            var inputs = {};
+            for (var argI in cmd.args) {
+                var arg = cmd.args[argI];
+                var inp = null;
+                if (arg.type == 'select') {
+                    inp = $('<select>').attr('name', arg.name);
+                    for (var optI in arg.options) {
+                        var opt = arg.options[optI];
+                        var optH = $('<option>').val(opt).append(opt);
+                        inp.append(optH);
+                    }
+                } else {
+                    inp = $('<input>');
+                    for (var paramI in arg) {
+                        inp.attr(paramI, arg[paramI]);
+                    }
+                }
+                if (cmd.autoEx) {
+                    inp.change(execCmd);
+                }
+                inputs[arg.name] = inp;
+            }
+
+            var isText = true;
+            var nameSplit = cmd.name.split('%');
+            for (var nsI in nameSplit) {
+                var ns = nameSplit[nsI];
+                form.append(isText ? ns : inputs[ns]);
+                isText = !isText;
+            }
+        }
+
+        if (!cmd.autoEx) {
+            form.append($('<input>')
+                .attr('type', 'submit')
+                .val(cmd.args ? 'Execute' : cmd.name)
+                .click(execCmd));
+        }
+        $('#globalCommands').append(form);
     }
 });
